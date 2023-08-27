2b1ed6a42ca0ee7b0219f955c9a726980b97504c
Nicolas Primo <nicolasprimo@gmail.com>
2023-08-27 11:34:16 -0300

    fixing and improvements

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,57 @@ export default function App() {
     setLoadedVideos(loadedVideos + 1);
   };
 
+  const [simba, setSimba] = useState([
+    ["img", "./5.jpg"],
+    ["video", "./30.mp4"],
+    ["img", "./6.jpg"],
+    ["img", "./38.jpg"],
+    ["video", "./27.mp4"],
+    ["img", "./8.jpg"],
+    ["img", "./37.jpg"],
+    ["video", "./21.mp4"],
+    ["img", "./9.jpg"],
+    ["video", "./28.mp4"],
+    ["img", "./36.jpg"],
+    ["video", "./29.mp4"],
+    ["img", "./13.jpg"],
+    ["video", "./39.mp4"],
+    ["video", "./45.mp4"],
+    ["img", "./46.jpg"],
+    ["img", "./47.jpg"],
+  ]);
+  const [nicolas, setNicolas] = useState([
+    ["img", "./44.jpg"],
+    ["img", "./43.jpg"],
+    ["img", "./42.jpg"],
+    ["img", "./41.jpg"],
+    ["img", "./40.jpg"],
+    ["img", "./7.jpg"],
+    ["img", "./14.jpg"],
+    ["img", "./31.jpg"],
+    ["img", "./32.jpg"],
+    ["img", "./33.jpg"],
+    ["img", "./15.jpg"],
+    ["img", "./16.jpg"],
+    ["video", "./22.mp4"],
+    ["video", "./35.mp4"],
+    ["video", "./34.mp4"],
+    ["img", "./17.jpg"],
+  ]);
+  const [us, setUs] = useState([
+    ["img", "./49.jpg"],
+    ["img", "./1.jpg"],
+    ["img", "./26.jpg"],
+    ["img", "./2.jpg"],
+    ["img", "./3.jpg"],
+    ["img", "./10.jpg"],
+    ["video", "./19.mp4"],
+    ["img", "./11.jpg"],
+    ["img", "./12.jpg"],
+    ["video", "./18.mp4"],
+    ["img", "./48.jpg"],
+  ]);
+
   return (
     <div className="flex flex-wrap h-full w-full lg:w-screen lg:h-screen  items-center justify-center gap-20 mt-32 mb-32 md:mt-12 md:mb-12 lg:mb-0 lg:mt-0 md:gap-[250px] md:flex-row ">
       <div className="flex flex-col items-center justify-center md:scale-130 lg:scale-150">
@@ -29,63 +80,28 @@ export default function App() {
           modules={[EffectCards]}
           className="w-[200px] h-[200px] "
         >
-          <SwiperSlide className=" rounded-2xl">
-            <img src="./1.jpg" alt="" />
-          </SwiperSlide>
-          <SwiperSlide className=" rounded-2xl">
-            <img
-              src="./26.jpg"
-              alt=""
-              className="object-cover h-[200px] w-[240px]"
-            />
-          </SwiperSlide>
-
-          <SwiperSlide className="rounded-2xl">
-            <img src="./2.jpg" alt="" />
-          </SwiperSlide>
-          <SwiperSlide className="rounded-2xl">
-            <video
-              src="./18.mp4"
-              autoPlay
-              loop
-              muted
-              preload="metadata"
-              playsInline
-              controlsList="false"
-              onLoadedData={handleVideoLoaded}
-              className="object-cover h-[225px] w-full"
-            />
-          </SwiperSlide>
-          <SwiperSlide className=" rounded-2xl">
-            <img
-              src="./3.jpg"
-              alt=""
-              className="object-cover h-[200px] w-full"
-            />
-          </SwiperSlide>
-
-          <SwiperSlide className="rounded-2xl">
-            <img src="./10.jpg" alt="" />
-          </SwiperSlide>
-          <SwiperSlide className="rounded-2xl">
-            <video
-              src="./19.mp4"
-              loop
-              autoPlay
-              muted
-              preload="metadata"
-              playsInline
-              controlsList="false"
-              onLoadedData={handleVideoLoaded}
-              className="object-cover w-full h-[200px]"
-            />
-          </SwiperSlide>
-          <SwiperSlide className="rounded-2xl">
-            <img src="./11.jpg" alt="" />
-          </SwiperSlide>
-          <SwiperSlide className="rounded-2xl">
-            <img src="./12.jpg" alt="" />
-          </SwiperSlide>
+          {us.map((data, key) => (
+            <SwiperSlide key={key} className="rounded-2xl">
+              {data[0] == "img" ? (
+                <img
+                  src={data[1]}
+                  className="object-cover h-[200px] w-[240px]"
+                />
+              ) : (
+                <video
+                  src={data[1]}
+                  autoPlay
+                  loop
+                  muted
+                  preload="metadata"
+                  playsInline
+                  controlsList="false"
+                  onLoadedData={handleVideoLoaded}
+                  className="object-cover h-[225px] w-full"
+                />
+              )}
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
 
@@ -99,102 +115,30 @@ export default function App() {
           modules={[EffectCards]}
           className="w-[200px] h-[200px]"
         >
-          <SwiperSlide className=" rounded-2xl">
-            <img
-              src="./7.jpg"
-              alt=""
-              className="h-[200px] object-cover w-full"
-            />
-          </SwiperSlide>
-          <SwiperSlide className="rounded-2xl">
-            <img
-              src="./14.jpg"
-              alt=""
-              className="h-[200px] object-cover w-full"
-            />
-          </SwiperSlide>
-          <SwiperSlide className=" rounded-2xl">
-            <img
-              src="./31.jpg"
-              alt=""
-              className="object-cover h-[280px] w-[240px]"
-            />
-          </SwiperSlide>
-          <SwiperSlide className=" rounded-2xl">
-            <img
-              src="./32.jpg"
-              alt=""
-              className="object-cover h-[200px] w-[240px]"
-            />
-          </SwiperSlide>
-          <SwiperSlide className=" rounded-2xl">
-            <img
-              src="./33.jpg"
-              alt=""
-              className="object-cover h-[200px] w-[240px]"
-            />
-          </SwiperSlide>
-          <SwiperSlide className=" rounded-2xl">
-            <img
-              src="./15.jpg"
-              alt=""
-              className="object-cover h-[200px] w-full"
-            />
-          </SwiperSlide>
-
-          <SwiperSlide className="rounded-2xl">
-            <img
-              src="./16.jpg"
-              alt=""
-              className="object-cover h-[200px] w-full"
-            />
-          </SwiperSlide>
-          <SwiperSlide className="rounded-2xl">
-            <video
-              src="./22.mp4"
-              autoPlay
-              loop
-              muted
-              preload="metadata"
-              playsInline
-              controlsList="false"
-              onLoadedData={handleVideoLoaded}
-              className="object-cover h-[200px] w-full"
-            />
-          </SwiperSlide>
-          <SwiperSlide className="rounded-2xl">
-            <video
-              src="./35.mp4"
-              autoPlay
-              loop
-              muted
-              preload="metadata"
-              playsInline
-              controlsList="false"
-              onLoadedData={handleVideoLoaded}
-              className="object-cover h-[200px] w-full"
-            />
-          </SwiperSlide>
-          <SwiperSlide className="rounded-2xl">
-            <video
-              src="./34.mp4"
-              autoPlay
-              loop
-              muted
-              preload="metadata"
-              playsInline
-              controlsList="false"
-              onLoadedData={handleVideoLoaded}
-              className="object-cover h-[200px] w-full"
-            />
-          </SwiperSlide>
-          <SwiperSlide className="rounded-2xl">
-            <img
-              src="./17.jpg"
-              alt=""
-              className="object-cover h-[200px] w-full"
-            />
-          </SwiperSlide>
+          {nicolas.map((data, key) => (
+            <SwiperSlide key={key} className="rounded-2xl">
+              {data[0] == "img" ? (
+                <img
+                  src={data[1]}
+                  className={`object-cover h-[200px] w-[240px] ${
+                    key == 7 ? "h-[280px]" : ""
+                  } ${key == 1 ? "h-[300px] " : ""}`}
+                />
+              ) : (
+                <video
+                  src={data[1]}
+                  autoPlay
+                  loop
+                  muted
+                  preload="metadata"
+                  playsInline
+                  controlsList="false"
+                  onLoadedData={handleVideoLoaded}
+                  className="object-cover h-[225px] w-full"
+                />
+              )}
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
 
@@ -208,106 +152,32 @@ export default function App() {
           modules={[EffectCards]}
           className="w-[200px] h-[200px]"
         >
-          <SwiperSlide className=" rounded-2xl">
-            <img
-              src="./5.jpg"
-              alt=""
-              className="h-[400px] object-cover w-full"
-            />
-          </SwiperSlide>
-          <SwiperSlide className="rounded-2xl">
-            <video
-              src="./30.mp4"
-              autoPlay
-              loop
-              muted
-              preload="metadata"
-              playsInline
-              controlsList="false"
-              onLoadedData={handleVideoLoaded}
-              className="object-cover h-[225px] w-full"
-            />
-          </SwiperSlide>
-          <SwiperSlide className="rounded-2xl">
-            <img
-              src="./6.jpg"
-              alt=""
-              className="h-[200px] object-cover w-full"
-            />
-          </SwiperSlide>
-          <SwiperSlide className="rounded-2xl">
-            <video
-              src="./27.mp4"
-              autoPlay
-              loop
-              muted
-              preload="metadata"
-              playsInline
-              controlsList="false"
-              onLoadedData={handleVideoLoaded}
-              className="object-cover h-[225px] w-full"
-            />
-          </SwiperSlide>
-          <SwiperSlide className=" rounded-2xl">
-            <img
-              src="./8.jpg"
-              alt=""
-              className="object-cover h-[200px] w-full"
-            />
-          </SwiperSlide>
-          <SwiperSlide className="rounded-2xl">
-            <video
-              src="./21.mp4"
-              autoPlay
-              loop
-              muted
-              preload="metadata"
-              playsInline
-              controlsList="false"
-              onLoadedData={handleVideoLoaded}
-              className="object-cover h-[200px] w-full"
-            />
-          </SwiperSlide>
-          <SwiperSlide className="rounded-2xl">
-            <img
-              src="./9.jpg"
-              alt=""
-              className="object-cover h-[200px] w-full"
-            />
-          </SwiperSlide>
-          <SwiperSlide className="rounded-2xl">
-            <video
-              src="./28.mp4"
-              autoPlay
-              loop
-              muted
-              preload="metadata"
-              playsInline
-              controlsList="false"
-              onLoadedData={handleVideoLoaded}
-              className="object-cover h-[225px] w-full"
-            />
-          </SwiperSlide>
-          <SwiperSlide className="rounded-2xl">
-            <video
-              src="./29.mp4"
-              autoPlay
-              loop
-              muted
-              preload="metadata"
-              playsInline
-              controlsList="false"
-              onLoadedData={handleVideoLoaded}
-              className="object-cover h-[225px] w-full"
-            />
-          </SwiperSlide>
-          <SwiperSlide className="rounded-2xl">
-            <img
-              src="./13.jpg"
-              alt=""
-              className="object-cover h-[200px] w-full"
-            />
-          </SwiperSlide>
+          {simba.map((data, key) => (
+            <SwiperSlide key={key} className="rounded-2xl">
+              {data[0] == "img" ? (
+                <img
+                  src={data[1]}
+                  className={`object-cover ${key == 0 ? "h-[400px]" : ""} ${
+                    key == 3 ? "h-[300px]" : ""
+                  } ${key == 15 ? "h-[300px]" : ""} ${
+                    key == 16 ? "h-[300px]" : ""
+                  } h-[200px] w-[240px]`}
+                />
+              ) : (
+                <video
+                  src={data[1]}
+                  autoPlay
+                  loop
+                  muted
+                  preload="metadata"
+                  playsInline
+                  controlsList="false"
+                  onLoadedData={handleVideoLoaded}
+                  className="object-cover h-[225px] w-full"
+                />
+              )}
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </div>
